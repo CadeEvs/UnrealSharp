8ec48046a76ee5eb7e9a514cb52b3d9f57a57127
cadeg <cadegriffin101@gmail.com>
2025-04-13 14:43:21 -0400

    Initial work adding K2_GetCategoryName

--- Source/UnrealSharpCore/Extensions/DeveloperSettings/CSDeveloperSettings.cpp
+++ Source/UnrealSharpCore/Extensions/DeveloperSettings/CSDeveloperSettings.cpp
@@ -1,1 +1,17 @@
 #include "CSDeveloperSettings.h"
+
+FName UCSDeveloperSettings::GetCategoryName() const
+{
+	FName Name = K2_GetCategoryName();
+	if (Name.IsNone())
+	{
+		return Super::GetCategoryName();
+	}
+
+	return Name;
+}
+
+FName UCSDeveloperSettings::K2_GetCategoryName_Implementation() const
+{
+	return FName();
+}
--- Source/UnrealSharpCore/Extensions/DeveloperSettings/CSDeveloperSettings.h
+++ Source/UnrealSharpCore/Extensions/DeveloperSettings/CSDeveloperSettings.h
@@ -8,9 +8,19 @@ UCLASS(Abstract)
 class UCSDeveloperSettings : public UDeveloperSettings
 {
 	GENERATED_BODY()
+
+public:
+	//~UDeveloperSettings interface
+	virtual FName GetCategoryName() const override;
+	//~End of UDeveloperSettings interface
+	
 #if WITH_EDITOR
 	// UDeveloperSettings interface
 	virtual bool SupportsAutoRegistration() const override { return false; }
 	// End of UDeveloperSettings interface
 #endif
+
+protected:
+	UFUNCTION(BlueprintNativeEvent, meta = (ScriptName = "GetCategoryName"), Category = "Managed Developer Settings")
+	FName K2_GetCategoryName() const;
 };
